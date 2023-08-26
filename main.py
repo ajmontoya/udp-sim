2b5e473d496dd956452c5d99f7db48614a0cff36
Alfredo Montoya <fredo.montoya@gmail.com>
2023-08-25 19:42:52 -0500

    Updated json payload to include 'params' obj

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import argparse
+from enum import IntEnum
 import json
+from pprint import pprint
 import random
 import socket
 import sys
 import time
 
 
-def cmdline_args() -> tuple[str, int, int, float, bool]:
+class Config(IntEnum):
+    SINGLE = 1
+    CROSS_02 = 2
+    CROSS_13 = 3
+    ALL_4 = 4
+
+
+def cmdline_args() -> tuple[str, int, int, int, int, list[int], float, bool]:
     parser = argparse.ArgumentParser(prog="udpsim", description="UDP Simulator")
 
     parser.add_argument(
@@ -26,7 +35,25 @@ def cmdline_args() -> tuple[str, int, int, float, bool]:
         "-t",
         "--timeout",
         type=int,
+        default=1,
         help="Num seconds to run simulator",
+    )
+    parser.add_argument(
+        "--power",
+        type=int,
+        default=20,
+        help="ESC power level",
+    )
+    parser.add_argument(
+        "--config",
+        type=int,
+        default=4,
+        help="Test configuration [1: single, 2: cross_02, 3: cross_13, 4: all_4]",
+    )
+    parser.add_argument(
+        "--step",
+        type=list[int],
+        help="Stepwise power intervals",
     )
     parser.add_argument(
         "-d",
@@ -45,14 +72,30 @@ def cmdline_args() -> tuple[str, int, int, float, bool]:
     args: argparse.Namespace = parser.parse_args()
     print(args) if args.verbose else None
 
-    return (args.address, args.port, args.timeout, args.delay, args.verbose)
+    return (
+        args.address,
+        args.port,
+        args.timeout,
+        args.power,
+        args.config,
+        args.step,
+        args.delay,
+        args.verbose,
+    )
 
 
-def gen_rand_data(testid: float, ts_idx: int, count: int) -> dict:
+def gen_rand_data(
+    testid: float, ts_idx: int, count: int, power: int, config: Config, step: bool
+) -> dict:
     return {
         "vehicle": "d126",
         "testid": testid,
         "escid": count,
+        "params": {
+            "power": power,
+            "config": config.value,
+            "step": step,
+        },
         "measurements": {
             "time": ts_idx,
             "rpm": random.randint(0, 5000),
@@ -74,7 +117,11 @@ def gen_rand_data(testid: float, ts_idx: int, count: int) -> dict:
 
 
 def main() -> None:
-    addr, port, timeout, delay, is_verbose = cmdline_args()
+    addr, port, timeout, power, config, step, delay, is_verbose = cmdline_args()
+
+    print(
+        f"address: {addr}, port: {port}, timeout: {timeout}, power: {power}, config: {config}, step: {step}, delay: {delay}, is verbose: {is_verbose}"
+    )
 
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print(client) if is_verbose else None
@@ -84,22 +131,42 @@ def main() -> None:
     try:
         t_start, t_diff = time.time(), 0
         count, ts_idx = 0, 1
+        step_enabled = True if step else False
+        cfg = Config(config)
 
-        while timeout is None or t_diff < timeout:
-            data = gen_rand_data(t_start, ts_idx, count)
-            print(data) if is_verbose else None
+        if step:
+            for powerStep in step:
+                data = gen_rand_data(
+                    t_start, ts_idx, count, powerStep, cfg, step_enabled
+                )
+                pprint(data) if is_verbose else None
 
-            payload = json.dumps(data).encode()
-            client.sendto(payload, (addr, port))
+                payload = json.dumps(data).encode()
+                client.sendto(payload, (addr, port))
 
-            time.sleep(delay)
-            t_diff = time.time() - t_start
+                time.sleep(delay)
 
-            if count < 3:
-                count += 1
-            else:
-                count = 0
-                ts_idx += 1
+                if count < 3:
+                    count += 1
+                else:
+                    count = 0
+                    ts_idx += 1
+        else:
+            while timeout is None or t_diff < timeout:
+                data = gen_rand_data(t_start, ts_idx, count, power, cfg, step_enabled)
+                pprint(data) if is_verbose else None
+
+                payload = json.dumps(data).encode()
+                client.sendto(payload, (addr, port))
+
+                time.sleep(delay)
+                t_diff = time.time() - t_start
+
+                if count < 3:
+                    count += 1
+                else:
+                    count = 0
+                    ts_idx += 1
 
     except KeyboardInterrupt:
         print("Closing socket and exiting...")
